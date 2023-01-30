319f554458b8b8d693b3ee1686e9013925258820
JoniJane <118185631+JoniJane@users.noreply.github.com>
2023-01-31 02:41:28 +0600

    Add files via upload

---
+++ tiy_week1/ifelse/1.py
@@ -0,0 +1,43 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+if a > b: print("a is greater than b")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
